639321ed408470807d6329eaed1ab9ddbde100b0
Bogdan Anokhin <Bogdan.Anokhin@tptlive.ee>
2024-12-16 11:29:59 +0200

    add es modules example

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
+//import common from './common.js';
+//common.hello();
+//import es from './es.js'
+//es.hello();
+
 const common = require('./common.js');
 common.hello();
+const es = requrire('./es.js');
+es.hello();
